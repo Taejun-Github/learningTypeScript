d3379cf3c4c16d25a50cc122746589b6d66d81f2
Taejun Kim <tjkimin2021@gmail.com>
2022-11-17 08:41:16 +0900

    타입스크립트 강좌 완료

--- 03purets/dist/detection.js
+++ 03purets/dist/detection.js
@@ -19,3 +19,43 @@ function isAdminAccount(account) {
     }
     return false;
 }
+function logValue(x) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    }
+    else {
+        console.log(x.toUpperCase());
+    }
+}
+function isFish(pet) {
+    return pet.swim !== undefined; // 이전에 정해주지 않고 일단 Fish라고 단정해본다. type predicate
+}
+function getFood(pet) {
+    if (isFish(pet)) {
+        pet;
+        return "fish food";
+    }
+    else {
+        pet;
+        return "bird food";
+    }
+}
+// function getTrueShape(shape: Shape) {
+//     if(shape.kind === "circle") {
+//         return Math.PI * shape.radius ** 2;
+//     }
+//     return shape.side * shape.side;
+// }
+function getArea(shape) {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2;
+        case "square":
+            return shape.side * shape.side;
+        case "rectagle":
+            return shape.length * shape.width;
+        default:
+            const _defaultforshape = shape;
+            return _defaultforshape;
+    }
+}
--- 03purets/src/detection.ts
+++ 03purets/src/detection.ts
@@ -31,4 +31,74 @@ function isAdminAccount(account: User | Admin): boolean {
     return false;
 } 
 
+function logValue(x: Date | string) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    } else {
+        console.log(x.toUpperCase());
+    }
+}
+
+type Fish = {
+    swim: () => void;
+}
+
+type Bird = {
+    fly: () => void;
+}
+
+function isFish(pet: Fish | Bird): pet is Fish {
+    return (pet as Fish).swim !== undefined; // 이전에 정해주지 않고 일단 Fish라고 단정해본다. type predicate
+}
+
+function getFood(pet: Fish | Bird) {
+    if (isFish(pet)) {
+        pet
+        return "fish food"
+    } else {
+        pet
+        return "bird food";
+    }
+}
+
+interface Circle {
+    kind: "circle",
+    radius: number,
+}
+
+interface Square {
+    kind: "square",
+    side: number,
+}
+
+interface Rectangle {
+    kind: "rectagle",
+    length: number,
+    width: number,
+}
+
+type Shape = Circle | Square | Rectangle
+
+// function getTrueShape(shape: Shape) {
+//     if(shape.kind === "circle") {
+//         return Math.PI * shape.radius ** 2;
+//     }
+//     return shape.side * shape.side;
+// }
+
+function getArea(shape: Shape) {
+    switch(shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2;
+        case "square":
+            return shape.side * shape.side;
+        case "rectagle":
+            return shape.length * shape.width;
+        default:
+            const _defaultforshape: never = shape;
+            return _defaultforshape;
+    }
+}
+
+
 export {}
